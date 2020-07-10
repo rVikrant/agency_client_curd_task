50cbc523642a40ee2f1c30bf91e6f1e4a763f1fc
rVikrant <vrao4557@gmail.com>
2020-07-10 16:00:11 +0530

    availability conflict resolved

--- config/db-config.js
+++ config/db-config.js
@@ -2,7 +2,7 @@
 
 let MONGO_URI	= `mongodb://${process.env.MONGO_HOST}:27017/${process.env.MONGO_DB_NAME}`;
 
-if(process.env.MONGO_USER_NAME && MONGO_USER_PASSWORD){
+if(process.env.MONGO_USER_NAME && process.env.MONGO_USER_PASSWORD){
 	MONGO_URI = `mongodb://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASSWORD}@${process.env.MONGO_HOST}:27017/${process.env.MONGO_DB_NAME}`;
 }
 
